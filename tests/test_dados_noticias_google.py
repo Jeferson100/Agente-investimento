d9c4abea1a0035ddcf561fffeab5cedd5e096f58
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2025-03-10 16:55:58 -0300

    Tentando corrigir problema na senha serper

--- tests/test_dados_noticias_google.py
+++ tests/test_dados_noticias_google.py
@@ -1,11 +1,18 @@
 from pydantic import SecretStr
 from coleta_dados.dados_noticias_google import DadosNoticiasGoogle
 import os
+from unittest.mock import patch
+from typing import Dict, List
+
+api_key_serper = os.getenv("API_KEY_SERPER")
 
 
 def test_get_news() -> None:
-    api_key_serper = os.environ.get("API_KEY_SERPER")
-    assert api_key_serper is not None, "API_KEY_SERPER environment variable is not set"
+    api_key_serper = os.getenv("API_KEY_SERPER")
+    
+    if api_key_serper is None:
+        raise ValueError("API key serper inválida ou não definida")
+
     obj = DadosNoticiasGoogle("PETR4", api_serper=SecretStr(api_key_serper))
     noticias = obj.get_news()
 
